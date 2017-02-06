e2a43a7a246ddea57c1d459b8f418febf82d2261
Monteven <Monteven@users.noreply.github.com>
2017-02-06 16:11:53 +0000

    Added comments

--- maths_cone_efficiency.py
+++ maths_cone_efficiency.py
@@ -1,27 +1,28 @@
+# Python program to calculate the most efficient height for the least surface area and a volume of 1 litre.
 import math
 from time import time
 lsa = []
 lh = []
 lr = []
-res = int(input("Desired Resolution (1 = 0dp, 10 = 1dp, 100 = 2dp, etc.): "))
+res = int(input("Desired Resolution (1 = 0dp, 10 = 1dp, 100 = 2dp, etc.): ")) # Lazy way to get the desired res.
 
 
 def main():
-    r = math.sqrt((3000/math.pi) / h)
-    l = math.sqrt(h**2 + r**2)
-    rsq = r**2
-    v = 0.33333333333333333333*math.pi*rsq*h
-    sa = math.pi*r*l
-    if 999.99 < v < 1000.01 and 8.9 < r < 9.1:
+    r = math.sqrt((3000/math.pi) / h) # Defining radius
+    l = math.sqrt(h**2 + r**2) # Defining length
+    rsq = r**2 # Defining radius^2
+    v = 0.33333333333333333333*math.pi*rsq*h # Defining volume, didn't like 1/3 so gave 0.333...
+    sa = math.pi*r*l # Surface area calculation
+    if 999.99 < v < 1000.01 and 8.9 < r < 9.1: # Filter for only some specific values close to desires value, bounds
         lsa.append(sa)
         lh.append(h)
         lr.append(r)
-t = time()
-for i in range(11*res, (13*res)):
+t = time() # Used to calculate time taken (I personally use this program to test efficiency methods as well as hardware)
+for i in range(11*res, (13*res)): # Going through the values
     h = i/res
     main()
 
-tfinal = time()-t
+tfinal = time()-t # Time taken
 lsas = lsa
 lsas.sort()
 bsa = lsas[0]
