076dd4f7d79f97f496d59303de627b86f3142b6d
Monteven <montaguemoran01@gmail.com>
2018-02-26 17:54:52 +0000

    Uploading auto_hypothesis_testing.py

---
+++ HypothesisTestingAuto/auto_hypothesis_testing.py
@@ -0,0 +1,94 @@
+import math
+
+# Defining vars
+hyp_sided = ""
+hyp_null_prob = ""
+drv = ""
+trials = ""
+sig_level = ""
+# Defining functions
+
+
+def ncr(n, r):
+    f = math.factorial
+    return f(n) // f(r) // f(n-r)
+
+def binomial(n,r):
+    b = ncr(n, r)*(hyp_null_prob**r)*((1-hyp_null_prob)**(trials-r))
+    return b
+
+def binomial_cumulative(n, r):
+    sum = 0
+    for i in range(0, r+1):
+        sum += binomial(n, i)
+    return sum
+
+
+def start(sided):
+    print("Let X be the DRV that represents", drv + ".")
+    print("\nX ~ B(" + str(trials) + ",", str(hyp_null_prob) + ")")
+    print("\nH0: p =", str(hyp_null_prob) + "                        H1: p", sided, str(hyp_null_prob))
+    print("\nLet c be the critical value.\n")
+
+
+def lower_side():
+    print("P(X<=c) <", sig_level_dec)
+
+    critical_value = 0
+    for i in range(0, trials+1):
+        b = binomial_cumulative(trials, i)
+        if b > sig_level_dec:
+            critical_value = i-1
+            critical_value_above = i
+            break
+    print("P(X<=" + str(critical_value) + ") =", str(format(binomial_cumulative(trials, critical_value), '.4f')))
+    print("P(X<=" + str(critical_value_above) + ") =", str(format(binomial_cumulative(trials, critical_value_above), '.4f')))
+    print("\nc =", str(critical_value))
+
+
+def upper_side():
+    print("P(X>=c) <", sig_level_dec)
+    print("1-P(X<=c-1) <", sig_level_dec)
+    print("P(X<=c-1) >", 1-sig_level_dec)
+
+    critical_value = 0
+    for i in range(0, trials+1):
+        b = binomial_cumulative(trials, i)
+        if b > (1-sig_level_dec):
+            critical_value = i
+            critical_value_below = i-1
+            break
+    print("P(X<=" + str(critical_value) + ") =", str(ncr(trials, critical_value)*(hyp_null_prob**critical_value)*((1-hyp_null_prob)**(trials-critical_value))))
+    print("P(X<=" + str(critical_value_below) + ") =", str(ncr(trials, critical_value_below)*(hyp_null_prob**(critical_value_below))*((1-hyp_null_prob)**(trials-critical_value_below))))
+    print("\nc-1 =", str(critical_value))
+    print("c =", str(critical_value+1))
+
+
+
+easy_mode_enabled = input("Easy or complex mode? (e/c, new users select e): ")
+
+
+if easy_mode_enabled == 'e':
+    hyp_sided = input("\nLower/Upper/Two Sided test? (l/u/t): ")
+    hyp_null_prob = float(input("\nNull hypothesis prob (e.g. 0.5) = "))
+    drv = input("\nWhat is the variable (e.g. number of heads thrown): ")
+    trials = int(input("\nHow many trials? "))
+    sig_level = float(input("\nWhat is the level of significance in %? "))
+
+    sig_level_dec = sig_level/100
+
+    print("\n--------------------------------------------------------------------------------\n")
+
+    if hyp_sided == "l":
+        start("<")
+        lower_side()
+    elif hyp_sided == "u":
+        start(">")
+        upper_side()
+    elif hyp_sided == "t":
+        start("!=")
+        sig_level_dec = sig_level_dec / 2
+        print("\nLower:\n")
+        lower_side()
+        print("\nUpper:\n")
+        upper_side()
--- maths_cone_efficiency.py
+++ maths_cone_efficiency.py
@@ -1,30 +1,33 @@
+# Python program to calculate the most efficient height for the least surface area and a volume of 1 litre.
 import math
 from time import time
-lsa = []
-lh = []
-lr = []
-res = int(input("Desired Resolution (1 = 0dp, 10 = 1dp, 100 = 2dp, etc.): "))
+surface_area_list = []
+height_list = []
+radius_list = []
+res = int(input("Desired Resolution (1 = 0dp, 10 = 1dp, 100 = 2dp, etc.): "))  # Lazy way to get the desired res.
 
 
 def main():
-    r = math.sqrt((3000/math.pi) / h)
-    l = math.sqrt(h**2 + r**2)
-    rsq = r**2
-    v = 0.33333333333333333333*math.pi*rsq*h
-    sa = math.pi*r*l
-    if 999.99 < v < 1000.01 and 8.9 < r < 9.1:
-        lsa.append(sa)
-        lh.append(h)
-        lr.append(r)
-t = time()
-for i in range(11*res, (13*res)):
+    r = math.sqrt((3000/math.pi) / h)  # Defining radius
+    l = math.sqrt(h**2 + r**2)  # Defining length
+    rsq = r**2  # Defining radius^2
+    v = (math.pi*rsq*h)/3  # Defining volume
+    sa = math.pi*r*l  # Surface area calculation
+    if 999.99 < v < 1000.01 and 8.9 < r < 9.1:  # Filter for only some specific values close to desires value, bounds
+        surface_area_list.append(sa)
+        height_list.append(h)
+        radius_list.append(r)
+time = time()  # Used to calculate time taken (I use this program to test efficiency methods as well as hardware)
+for i in range(11*res, (13*res)):  # Going through the values
     h = i/res
     main()
 
-tfinal = time()-t
-lsas = lsa
-lsas.sort()
-bsa = lsas[0]
-bh = lh[lsa.index(bsa)]
-br = lr[lsa.index(bsa)]
-print("\nBest surface area: " + str(bsa) + "\nBest Height: " + str(bh) + "\nBest Radius: " + str(br) + "\n\nTime: " + str(tfinal) + " seconds, or " + str(tfinal/60) + " minutes, or " + str((tfinal/60)/60) + " hours.")
+final_time = time()-time  # Time taken
+sorted_surface_area_list = surface_area_list
+sorted_surface_area_list.sort()
+best_surface_area = sorted_surface_area_list[0]
+best_height = height_list[surface_area_list.index(best_surface_area)]
+best_radius = radius_list[surface_area_list.index(best_surface_area)]
+print("\nBest surface area: " + str(best_surface_area) + "\nBest Height: " + str(best_height) +
+      "\nBest Radius: " + str(best_radius) + "\n\nTime: " + str(final_time) + " seconds, or " + str(final_time/60) +
+      " minutes, or " + str((final_time/60)/60) + " hours.")
